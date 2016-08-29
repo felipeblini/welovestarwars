dc5b9af489b3bec4f0b8315e9944c0298eafb829
Felipe blini <Felipe.blini@gmail.com>
2016-08-29 19:44:24 -0300

    instalado e inicializado bower, instalado gulp, instalado modulo do node wiredep e a task gulp-inject do gulp e instalado e injetado automaticamente materialize.css e angularjs

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
 // Load plugins
 var gulp = require('gulp');
+var wiredep = require('wiredep').stream;
 
-gulp.task('minhatask', function() {
-    return gulp.src('www/js/index.js')
-        .pipe(gulp.dest('./www'));
+
+gulp.task('wiredep', function() {
+    console.log('injetando dependencias css e js gerenciadas pelo bower');
+
+    return gulp.src('www/index.html')
+        .pipe(wiredep())
+        .pipe(gulp.dest('www'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('bower.json', ['wiredep']);
 })
